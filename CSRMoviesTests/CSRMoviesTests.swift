83be220a09262a2b5a8b6379e32ddf8c985465cb
Ryan Khalili <rykhalil4@gmail.com>
2015-03-05 10:15:28 -0800

    Initial Commit

---
+++ CSRMoviesTests/CSRMoviesTests.swift
@@ -0,0 +1,36 @@
+//
+//  CSRMoviesTests.swift
+//  CSRMoviesTests
+//
+//  Created by Ryan Khalili on 3/5/15.
+//  Copyright (c) 2015 CSR. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CSRMoviesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
